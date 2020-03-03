a83f1e084257aee98923cc151ee579e0c7a3c6bf
Fedorov113 <fedorov.de@gmail.com>
2020-03-04 00:21:18 +0300

    salmon

--- assnake_transcriptome/snake_module_setup.py
+++ assnake_transcriptome/snake_module_setup.py
@@ -5,12 +5,13 @@ from assnake.utils import read_yaml
 
 from assnake_transcriptome.invocation_commands import map_hisat2
 from assnake_transcriptome.invocation_commands import feature_counts
+from assnake_transcriptome.salmon.commands import salmon_invoke
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 snake_module = SnakeModule(name = 'assnake-transcriptome', 
                            install_dir = this_dir,
-                           snakefiles = ['./hisat2_workflow.smk'],
-                           invocation_commands = [map_hisat2, feature_counts],
+                           snakefiles = ['./hisat2_workflow.smk', './salmon/workflow.smk'],
+                           invocation_commands = [map_hisat2, feature_counts, salmon_invoke],
                            initialization_commands = [],
                            wc_configs = [
                             #    read_yaml(os.path.join(this_dir, './wc_config.yaml'))
